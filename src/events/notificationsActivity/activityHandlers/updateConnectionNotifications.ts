e73dfd9310aed6273d3e71cdc0ab45ddb7d8de17
Akash Krishnan <akashkrishnan98@gmail.com>
2022-04-10 10:58:44 +0530

    LOG: Catch and log error

--- src/events/notificationsActivity/activityHandlers/updateConnectionNotifications.ts
+++ src/events/notificationsActivity/activityHandlers/updateConnectionNotifications.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { v4 } from 'uuid';
 import DB_HELPERS from '../../../db/helpers';
 import { INotification } from '../../../db/models/notifications';
@@ -8,25 +9,29 @@ const updateConnectionNotifications = async (
     notificationActivity: INotificationActivity,
     connectionActivityType: IConnectionActivityType
 ) => {
-    const connectionResource = await DB_QUERIES.getConnection(notificationActivity.resourceRefId);
-    if (!connectionResource) {
-        //handle error
-        return;
-    }
-    const userNotificationBox = await DB_HELPERS.getUserNotificationsForceCreated(connectionResource.connectorId);
+    try {
+        const connectionResource = await DB_QUERIES.getConnection(notificationActivity.resourceRefId);
+        if (!connectionResource) {
+            //handle error
+            return;
+        }
+        const userNotificationBox = await DB_HELPERS.getUserNotificationsForceCreated(connectionResource.connectorId);
 
-    const notification: INotification = {
-        id: v4(),
-        createdAt: Date.now(),
-        expiresAt: Infinity,
-        read: false,
-        resourceType: connectionActivityType,
-        resourceRef: connectionResource.id,
-    };
-    await DB_QUERIES.updateUserNotification(userNotificationBox.id, [
-        notification,
-        ...userNotificationBox.notifications,
-    ]);
+        const notification: INotification = {
+            id: v4(),
+            createdAt: Date.now(),
+            expiresAt: Infinity,
+            read: false,
+            resourceType: connectionActivityType,
+            resourceRef: connectionResource.id,
+        };
+        await DB_QUERIES.updateUserNotification(userNotificationBox.id, [
+            notification,
+            ...userNotificationBox.notifications,
+        ]);
+    } catch (err: any) {
+        console.log(err.message);
+    }
     return;
 };
 
